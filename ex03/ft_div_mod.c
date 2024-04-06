5646ecf3fbfedd4d5d390869cecdc0eb9ca802f7
DiegoDev20 <dediegosc@gmail.com>
2024-04-06 13:31:02 -0500

    Add if

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -1,6 +1,10 @@
 
 void ft_div_mod(int a,int b,int *div,int *mod)
 {
+	if(b==0)
+	{
+		return;
+	}
 	*div=a/b;
 	*mod=a%b;
 }
--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -1,6 +1,10 @@
 
 void ft_ultimate_div_mod(int *a, int *b)
 {
+	if(*b==0)
+	{
+		return;
+	}
 	int num1=*a;
 	int num2=*b;
 	*a=num1/num2;
